9a055996c76c06aa081ff8785100009e9655f368
karunakarth <karunakar.th@gmail.com>
2020-04-04 11:00:33 +0530

    updating till now

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular: Getting Started';
+  pageTitle = 'Acme Product Management';
+
+  getTitle() {
+    return this.pageTitle;
+  }
 }
